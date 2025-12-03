ceeb63ce3cb7a0d181fb04a6f8161ae51256894a
Rizato <rldifts@gmail.com>
2025-12-03 14:35:12 -0500

    Add linear time greedy part two solution
    Uses a linked list to maintain a list of digits, with constant time
    deletion.
    
    At each step, it adds the next value to the end of the list, and updates
    the cache of potentially out of order nodes, then it removes the first
    out of order node, or removes the last inserted.
    
    A node is out of order if the node following it has a greater value. In
    this way, we always remove the value with the largest increase to the
    current value

--- aoc25/three/solution.py
+++ aoc25/three/solution.py
@@ -1,10 +1,61 @@
 import dataclasses
-from typing import List, Self
+from typing import Iterable, List, Optional, Self
+
+
+def list_to_int(digits: Iterable) -> int:
+    total = 0
+    for i in digits:
+        if i > 10:
+            continue
+        total *= 10
+        total += int(i)
+
+    return total
+
+
+class LinkedListIterator:
+    def __init__(self, head: Optional["ListNode"] = None):
+        self.head = head
+
+    def __next__(self):
+        if self.head is None:
+            raise StopIteration
+
+        value = self.head.value
+        self.head = self.head.next
+        return value
+
+
+class ListNode:
+    value: int
+    next: Optional["ListNode"] = None
+    prev: Optional["ListNode"] = None
+
+    def __init__(self, value):
+        self.value = value
+
+    def __iter__(self):
+        return LinkedListIterator(self)
+
+    def __repr__(self):
+        return f"ListNode({self.value})"
+
+
+def add_node_before(tail: ListNode, node: ListNode):
+    tail.prev.next = node
+    node.prev = tail.prev
+    tail.prev = node
+    node.next = tail
+
+
+def remove_node(node: ListNode):
+    node.prev.next = node.next
+    node.next.prev = node.prev
 
 
 @dataclasses.dataclass(frozen=True)
 class BatteryBank:
-    batteries: List[int]
+    cells: List[int]
 
     @classmethod
     def parse_bank(cls, value: str) -> Self:
@@ -12,72 +63,100 @@ class BatteryBank:
 
 
 class JoltageCalculator:
-    def __init__(self, active_cells: int):
-        self.active_cells = active_cells
+    def __init__(self, num_active_cells: int):
+        self.num_active_cells = num_active_cells
         self.total_joltage = 0
 
     def add_max_joltage(self, bank: BatteryBank):
-        self.total_joltage += self.find_max_joltage_part_two_dp(bank, self.active_cells)
+        self.total_joltage += self.find_max_joltage_part_two_linear(
+            bank, self.num_active_cells
+        )
 
     @staticmethod
-    def find_max_joltage(bank: BatteryBank) -> int:
+    def find_max_joltage_part_one(bank: BatteryBank) -> int:
         # This is a greedy algorithm because forced ltr
         tens_digit = 0
         ones_digit = 0
-        for i in range(len(bank.batteries)):
+        for i in range(len(bank.cells)):
             # Basically step up the 10s and ones digits as I go through if they are higher
             # Set the 10s digit if greater, and not the last number
-            if bank.batteries[i] > tens_digit and i + 1 < len(bank.batteries):
-                tens_digit = bank.batteries[i]
-                ones_digit = bank.batteries[i + 1]
-            elif bank.batteries[i] > ones_digit:
+            if bank.cells[i] > tens_digit and i + 1 < len(bank.cells):
+                tens_digit = bank.cells[i]
+                ones_digit = bank.cells[i + 1]
+            elif bank.cells[i] > ones_digit:
                 # set the ones digit if greater
-                ones_digit = bank.batteries[i]
+                ones_digit = bank.cells[i]
 
         return tens_digit * 10 + ones_digit
 
     @staticmethod
-    def find_max_joltage_part_two_dp(bank: BatteryBank, to_activate: int) -> int:
-        # activate only the active cells
-        # I believe this is a dynamic programming problem...
-        # At each stage we can cache the largest number to that point...
-        # we can work backwards to build the largest number
-        # dp = []
-        # for battery in reversed(bank.batteries):
-        # we want the maximum at every point while working forward
-        # if we have less than 12 length....
-
-        # if we have 12 length, replace 12th digit with self
-        # but that will just end up wrong, because we need to replace the right number
-        # so create the maximally large number with the smallest number of digits?
-
-        # alternatively, we could find the largest numbers between the start and final 11 numbers
-        # then, our search range is from last found + 1 to final 10 numbers (total - 1 - found)
-        # this will basically be O(n * 12)
-        # But to_activate is now a property, so O(n * m)
-        # not the fastest, but should work
+    def find_max_joltage_part_two_bf(bank: BatteryBank, num_to_activate: int) -> int:
+        # find the max number between the last max and the end - remaining digits to find
+        # so start 0 to len - 11, then found to len - 10
+        # O(n * m), since we do m steps, over (about) n entries
         front = 0
-        digits = [0] * to_activate
+        digits = [0] * num_to_activate
         # find to_activate digits
-        for i in range(to_activate):
+        for i in range(num_to_activate):
             # start from previous found, until the
             max_digit = 0
 
-            # Ensure that you always pick a number before the last 12
-            for j in range(front, len(bank.batteries) - to_activate + i + 1):
-                if bank.batteries[j] > max_digit:
-                    max_digit = bank.batteries[j]
+            # Ensure that you always pick a number before the required number to fill out a full answer
+            for j in range(front, len(bank.cells) - num_to_activate + i + 1):
+                if bank.cells[j] > max_digit:
+                    max_digit = bank.cells[j]
                     digits[i] = max_digit
                     # Reduce the scope of the search from the next onward
                     front = j + 1
 
-        return sum(i * 10**power for power, i in enumerate(reversed(digits)))
+        return list_to_int(digits)
+
+    @staticmethod
+    def find_max_joltage_part_two_linear(
+        bank: BatteryBank, num_to_activate: int
+    ) -> int:
+        # find the max when activating a given number of cells
+        # This greedy algorithm runs in O(n) time
+        # It iterates across the bank, maintaining a linked list of the digits, and a cache of potentially out of order digits
+
+        # sentinel node at head and tail
+        head = ListNode(99)
+        tail = ListNode(99)
+        head.next = tail
+        tail.prev = head
+
+        # cache which nodes have a value less than their follower
+        removable_cells = []
+
+        for i, cell in enumerate(bank.cells):
+            # insert new battery at the end
+            node = ListNode(cell)
+            add_node_before(tail, node)
+            if node.prev.value < node.value:
+                removable_cells.append(node.prev)
+
+            # don't remove any until we have a full set of digits
+            if i < num_to_activate:
+                continue
+
+            # remove the first node that is less than a following node
+            try:
+                to_remove = removable_cells.pop(0)
+                remove_node(to_remove)
+                # Check if previous node can now be removed
+                if to_remove.prev.value < to_remove.next.value:
+                    removable_cells.insert(0, to_remove.prev)
+            except IndexError:
+                # Remove newly added node last if canRemove is empty
+                remove_node(node)
+
+        return list_to_int(head)
 
 
 def run() -> int:
     calc = JoltageCalculator(12)
     with open("input.txt", "r") as f:
-        for line in f.readlines():
+        for line in f:
             if not line.strip():
                 continue
 
--- aoc25/three/test_solution.py
+++ aoc25/three/test_solution.py
@@ -2,48 +2,153 @@ from aoc25.three.solution import BatteryBank, JoltageCalculator
 
 
 def test_part_one_examples():
-    assert 98 == JoltageCalculator.find_max_joltage(
-        BatteryBank.parse_bank("987654321111111")
+    assert (
+        JoltageCalculator.find_max_joltage_part_one(
+            BatteryBank.parse_bank("987654321111111")
+        )
+        == 98
     )
-    assert 89 == JoltageCalculator.find_max_joltage(
-        BatteryBank.parse_bank("811111111111119")
+    assert (
+        JoltageCalculator.find_max_joltage_part_one(
+            BatteryBank.parse_bank("811111111111119")
+        )
+        == 89
     )
-    assert 78 == JoltageCalculator.find_max_joltage(
-        BatteryBank.parse_bank("234234234234278")
+    assert (
+        JoltageCalculator.find_max_joltage_part_one(
+            BatteryBank.parse_bank("234234234234278")
+        )
+        == 78
     )
-    assert 92 == JoltageCalculator.find_max_joltage(
-        BatteryBank.parse_bank("818181911112111")
+    assert (
+        JoltageCalculator.find_max_joltage_part_one(
+            BatteryBank.parse_bank("818181911112111")
+        )
+        == 92
     )
 
 
 def test_part_two_examples():
-    assert 987654321111 == JoltageCalculator.find_max_joltage_part_two_dp(
-        BatteryBank.parse_bank("987654321111111"), 12
+    assert (
+        JoltageCalculator.find_max_joltage_part_two_bf(
+            BatteryBank.parse_bank("987654321111111"), 12
+        )
+        == 987654321111
     )
-    assert 811111111119 == JoltageCalculator.find_max_joltage_part_two_dp(
-        BatteryBank.parse_bank("811111111111119"), 12
+    assert (
+        JoltageCalculator.find_max_joltage_part_two_bf(
+            BatteryBank.parse_bank("811111111111119"), 12
+        )
+        == 811111111119
     )
-    assert 434234234278 == JoltageCalculator.find_max_joltage_part_two_dp(
-        BatteryBank.parse_bank("234234234234278"), 12
+    assert (
+        JoltageCalculator.find_max_joltage_part_two_bf(
+            BatteryBank.parse_bank("234234234234278"), 12
+        )
+        == 434234234278
     )
-    assert 888911112111 == JoltageCalculator.find_max_joltage_part_two_dp(
-        BatteryBank.parse_bank("818181911112111"), 12
+    assert (
+        JoltageCalculator.find_max_joltage_part_two_bf(
+            BatteryBank.parse_bank("818181911112111"), 12
+        )
+        == 888911112111
     )
 
 
 def test_part_two_last_11_max():
-    assert 199999999999 == JoltageCalculator.find_max_joltage_part_two_dp(
-        BatteryBank.parse_bank("1111199999999999"), 12
+    assert (
+        JoltageCalculator.find_max_joltage_part_two_bf(
+            BatteryBank.parse_bank("1111199999999999"), 12
+        )
+        == 199999999999
     )
 
 
 def test_part_two_largest_last():
-    assert 567123456789 == JoltageCalculator.find_max_joltage_part_two_dp(
-        BatteryBank.parse_bank("1234567123456789"), 12
+    assert (
+        JoltageCalculator.find_max_joltage_part_two_bf(
+            BatteryBank.parse_bank("1234567123456789"), 12
+        )
+        == 567123456789
     )
 
 
 def test_part_two_largest_first():
-    assert 911111111111 == JoltageCalculator.find_max_joltage_part_two_dp(
-        BatteryBank.parse_bank("9111111111111111"), 12
+    assert (
+        JoltageCalculator.find_max_joltage_part_two_bf(
+            BatteryBank.parse_bank("9111111111111111"), 12
+        )
+        == 911111111111
+    )
+
+
+def test_part_two_examples_linear():
+    assert (
+        JoltageCalculator.find_max_joltage_part_two_linear(
+            BatteryBank.parse_bank("987654321111111"), 12
+        )
+        == 987654321111
+    )
+    assert (
+        JoltageCalculator.find_max_joltage_part_two_linear(
+            BatteryBank.parse_bank("811111111111119"), 12
+        )
+        == 811111111119
+    )
+    assert (
+        JoltageCalculator.find_max_joltage_part_two_linear(
+            BatteryBank.parse_bank("234234234234278"), 12
+        )
+        == 434234234278
+    )
+    assert (
+        JoltageCalculator.find_max_joltage_part_two_linear(
+            BatteryBank.parse_bank("818181911112111"), 12
+        )
+        == 888911112111
+    )
+
+
+def test_part_two_last_11_max_linear():
+    assert (
+        JoltageCalculator.find_max_joltage_part_two_linear(
+            BatteryBank.parse_bank("1111199999999999"), 12
+        )
+        == 199999999999
+    )
+
+
+def test_part_two_largest_last_linear():
+    assert (
+        JoltageCalculator.find_max_joltage_part_two_linear(
+            BatteryBank.parse_bank("1234567123456789"), 12
+        )
+        == 567123456789
+    )
+
+
+def test_part_two_largest_first_linear():
+    assert (
+        JoltageCalculator.find_max_joltage_part_two_linear(
+            BatteryBank.parse_bank("9111111111111111"), 12
+        )
+        == 911111111111
+    )
+
+
+def test_part_two_cycling_linear():
+    assert (
+        JoltageCalculator.find_max_joltage_part_two_linear(
+            BatteryBank.parse_bank("123456789098765432123456789"), 12
+        )
+        == 998765456789
+    )
+
+
+def test_part_two_multi_larger():
+    assert (
+        JoltageCalculator.find_max_joltage_part_two_linear(
+            BatteryBank.parse_bank("1123456789098765432123456789"), 12
+        )
+        == 998765456789
     )
