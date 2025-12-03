a3c6ee8f6733544682f1963d4136a067a6ff7bd4
Rizato <rldifts@gmail.com>
2025-12-03 12:04:53 -0500

    Add day 3 part two bf solution
    Brute force O(n*m) solution

--- aoc25/three/solution.py
+++ aoc25/three/solution.py
@@ -12,11 +12,12 @@ class BatteryBank:
 
 
 class JoltageCalculator:
-    def __init__(self):
+    def __init__(self, active_cells: int):
+        self.active_cells = active_cells
         self.total_joltage = 0
 
     def add_max_joltage(self, bank: BatteryBank):
-        self.total_joltage += self.find_max_joltage(bank)
+        self.total_joltage += self.find_max_joltage_part_two_dp(bank, self.active_cells)
 
     @staticmethod
     def find_max_joltage(bank: BatteryBank) -> int:
@@ -35,9 +36,46 @@ class JoltageCalculator:
 
         return tens_digit * 10 + ones_digit
 
+    @staticmethod
+    def find_max_joltage_part_two_dp(bank: BatteryBank, to_activate: int) -> int:
+        # activate only the active cells
+        # I believe this is a dynamic programming problem...
+        # At each stage we can cache the largest number to that point...
+        # we can work backwards to build the largest number
+        # dp = []
+        # for battery in reversed(bank.batteries):
+        # we want the maximum at every point while working forward
+        # if we have less than 12 length....
+
+        # if we have 12 length, replace 12th digit with self
+        # but that will just end up wrong, because we need to replace the right number
+        # so create the maximally large number with the smallest number of digits?
+
+        # alternatively, we could find the largest numbers between the start and final 11 numbers
+        # then, our search range is from last found + 1 to final 10 numbers (total - 1 - found)
+        # this will basically be O(n * 12)
+        # But to_activate is now a property, so O(n * m)
+        # not the fastest, but should work
+        front = 0
+        digits = [0] * to_activate
+        # find to_activate digits
+        for i in range(to_activate):
+            # start from previous found, until the
+            max_digit = 0
+
+            # Ensure that you always pick a number before the last 12
+            for j in range(front, len(bank.batteries) - to_activate + i + 1):
+                if bank.batteries[j] > max_digit:
+                    max_digit = bank.batteries[j]
+                    digits[i] = max_digit
+                    # Reduce the scope of the search from the next onward
+                    front = j + 1
+
+        return sum(i * 10**power for power, i in enumerate(reversed(digits)))
+
 
 def run() -> int:
-    calc = JoltageCalculator()
+    calc = JoltageCalculator(12)
     with open("input.txt", "r") as f:
         for line in f.readlines():
             if not line.strip():
--- aoc25/three/test_solution.py
+++ aoc25/three/test_solution.py
@@ -14,3 +14,36 @@ def test_part_one_examples():
     assert 92 == JoltageCalculator.find_max_joltage(
         BatteryBank.parse_bank("818181911112111")
     )
+
+
+def test_part_two_examples():
+    assert 987654321111 == JoltageCalculator.find_max_joltage_part_two_dp(
+        BatteryBank.parse_bank("987654321111111"), 12
+    )
+    assert 811111111119 == JoltageCalculator.find_max_joltage_part_two_dp(
+        BatteryBank.parse_bank("811111111111119"), 12
+    )
+    assert 434234234278 == JoltageCalculator.find_max_joltage_part_two_dp(
+        BatteryBank.parse_bank("234234234234278"), 12
+    )
+    assert 888911112111 == JoltageCalculator.find_max_joltage_part_two_dp(
+        BatteryBank.parse_bank("818181911112111"), 12
+    )
+
+
+def test_part_two_last_11_max():
+    assert 199999999999 == JoltageCalculator.find_max_joltage_part_two_dp(
+        BatteryBank.parse_bank("1111199999999999"), 12
+    )
+
+
+def test_part_two_largest_last():
+    assert 567123456789 == JoltageCalculator.find_max_joltage_part_two_dp(
+        BatteryBank.parse_bank("1234567123456789"), 12
+    )
+
+
+def test_part_two_largest_first():
+    assert 911111111111 == JoltageCalculator.find_max_joltage_part_two_dp(
+        BatteryBank.parse_bank("9111111111111111"), 12
+    )
