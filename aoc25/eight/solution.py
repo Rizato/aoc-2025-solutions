6edcc492a16808d096f7425fc518ef68ce99a715
Robert Lathrop <rldifts@gmail.com>
2025-12-08 12:31:21 -0500

    Add day eight part two solution

--- aoc25/eight/solution.py
+++ aoc25/eight/solution.py
@@ -30,18 +30,64 @@ class Graph:
         self.edges.append(list())
         self.num_vertices += 1
 
-    def get_n_shortest(self, n: int) -> List[Tuple[float, int, int]]:
+    def get_shortest(self) -> List[Tuple[float, int, int]]:
         distances: List[Tuple[float, int, int]] = []
-        for i, node in enumerate(self.vertices):
+        for i, source in enumerate(self.vertices):
             for j in range(i + 1, len(self.vertices)):
-                other = self.vertices[j]
-                distances.append((node.distance(other), i, j))
+                target = self.vertices[j]
+                distances.append((source.distance(target), i, j))
         # sort from shortest to longest, and take n connections
-        return sorted(distances, key=lambda x: x[0])[:n]
+        return sorted(distances, key=lambda x: x[0])
+
+    def create_single_circuit(self) -> int:
+        connections = deque(self.get_shortest())
+        connected = [False] * self.num_vertices
+
+        # start a circuit on the first connection
+        first = True
+        last_source = 0
+        last_target = 0
+
+        # mark both of their circuits connected if either is
+        def mark_circuit_connected(vertex: int):
+            """
+            bfs mark all connected vertices as connected
+            """
+            queue = deque([vertex])
+            discovered = [False] * self.num_vertices
+            discovered[vertex] = True
+            while queue:
+                current = queue.popleft()
+                connected[current] = True
+                for edge in self.edges[current]:
+                    if not discovered[edge]:
+                        discovered[edge] = True
+                        queue.append(edge)
+
+        while connections and not all(connected):
+            _, source, target = connections.popleft()
+            last_source = source
+            last_target = target
+            if first:
+                connected[source] = True
+                connected[target] = True
+                first = False
+
+            if connected[target] and not connected[source]:
+                mark_circuit_connected(source)
+
+            if connected[source] and not connected[target]:
+                mark_circuit_connected(target)
+
+            # add edge to graph
+            self.edges[source].append(target)
+            self.edges[target].append(source)
+
+        return self.vertices[last_source].x * self.vertices[last_target].x
 
     def connect_n_shortest(self, n: int) -> int:
         # Connect the n shortest edges
-        for _, node, other in self.get_n_shortest(n):
+        for _, node, other in self.get_shortest()[:n]:
             self.edges[node].append(other)
             self.edges[other].append(node)
 
@@ -100,7 +146,7 @@ def run() -> int:
     with open("input.txt", "r") as f:
         graph = JunctionParser().parse(f.read())
 
-    return graph.connect_n_shortest(1000)
+    return graph.create_single_circuit()
 
 
 if __name__ == "__main__":
