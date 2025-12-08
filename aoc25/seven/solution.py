01c8a6bba5382fa9ea598a11efc28cc389e72085
Robert Lathrop <rldifts@gmail.com>
2025-12-08 09:03:01 -0500

    Add day seven part two
    Also refactor part one to not modify state in place, but update the whole row at the end

--- aoc25/seven/solution.py
+++ aoc25/seven/solution.py
@@ -1,4 +1,5 @@
 import enum
+from functools import lru_cache
 from typing import List
 
 
@@ -7,6 +8,24 @@ class Tile(enum.Enum):
     SPLITTER = "^"
     SOURCE = "S"
     BEAM = "|"
+
+
+class TreeNode:
+
+    def __init__(self):
+        self.left = None
+        self.right = None
+
+
+@lru_cache(maxsize=None)
+def count_paths(root: TreeNode):
+    if not root:
+        return 0
+
+    if not root.left and not root.right:
+        return 1
+    else:
+        return count_paths(root.left) + count_paths(root.right)
 
 
 class TachyonField:
@@ -18,29 +37,68 @@ class TachyonField:
     def __init__(self, field: List[List[Tile]]):
         self.field: List[List[Tile]] = field
         self.width = len(field[0])
-        self.current_state: List[Tile] = [Tile.EMPTY] * self.width
 
     def solve(self) -> int:
         splits = 0
+        previous_state = [Tile.EMPTY] * self.width
         for row in self.field:
+            next_state = [Tile.EMPTY] * self.width
             for i, tile in enumerate(row):
-                current = self.current_state[i]
                 if tile == Tile.SOURCE:
-                    self.current_state[i] = Tile.BEAM
+                    next_state[i] = Tile.BEAM
                 elif tile == Tile.SPLITTER:
-                    if current == Tile.BEAM:
+                    next_state[i] = Tile.SPLITTER
+                    if previous_state[i] == Tile.BEAM:
                         splits += 1
-                        # beam stops, and moves left and right, if possible
-                        self.current_state[i] = Tile.SPLITTER
-                        if i > 0 and row[i - 1] != Tile.SPLITTER:
-                            self.current_state[i - 1] = Tile.BEAM
-                        if i < self.width - 1 and row[i + 1] != Tile.SPLITTER:
-                            self.current_state[i + 1] = Tile.BEAM
-                else:
-                    # NOP
-                    pass
+                        for j in (i - 1, i + 1):
+                            # beam stops, and moves left and right, if possible
+                            if 0 <= j < self.width and row[j] != Tile.SPLITTER:
+                                next_state[j] = Tile.BEAM
 
+                else:  # tile is empty
+                    next_state[i] = (
+                        previous_state[i]
+                        if previous_state[i] == Tile.BEAM
+                        else next_state[i]
+                    )
+
+            previous_state = next_state
         return splits
+
+    def solve_quantum_paths(self) -> int:
+        root = TreeNode()
+        current_state: List[List[TreeNode]] = [list() for _ in range(self.width)]
+        # Find source
+        source = self.field[0].index(Tile.SOURCE)
+        current_state[source].append(root)
+        for row in self.field[1:]:
+            # find all splitters, with TreeNodes in current_state
+            splitters = [
+                i
+                for i, tile in enumerate(row)
+                if tile == Tile.SPLITTER and current_state[i]
+            ]
+            # create a new node per new path, using set to remove dupes
+            tachyon_splits = {j for i in splitters for j in (i - 1, i + 1)}
+            for j in tachyon_splits:
+                tachyon = TreeNode()
+                if 0 <= j < self.width:
+                    current_state[j].append(tachyon)
+                    # Add a node to all parents in the state (from earlier splits leading to the same path)
+                    parent_left = j - 1
+                    if parent_left in splitters:
+                        for parent in current_state[parent_left]:
+                            # parent left, has this child in right slot
+                            parent.right = tachyon
+                    parent_right = j + 1
+                    if parent_right in splitters:
+                        for parent in current_state[parent_right]:
+                            parent.left = tachyon
+            for i in splitters:
+                # remove all the splitters, so they don't get split again
+                current_state[i].clear()
+
+        return count_paths(root)
 
 
 class TachyonParser:
@@ -64,7 +122,7 @@ def run() -> int:
     with open("input.txt", "r") as f:
         tachyon_field = TachyonParser().parse(f.read())
 
-    return tachyon_field.solve()
+    return tachyon_field.solve_quantum_paths()
 
 
 if __name__ == "__main__":
