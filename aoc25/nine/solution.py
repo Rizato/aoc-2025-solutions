e2eb52a90a811a997d522da48e8ee54c4f613b74
Robert Lathrop <rldifts@gmail.com>
2025-12-09 08:55:49 -0500

    Improve solution slightly
    Still O(n**2)

--- aoc25/nine/solution.py
+++ aoc25/nine/solution.py
@@ -2,11 +2,11 @@ import dataclasses
 from typing import List
 
 
-
 @dataclasses.dataclass(frozen=True)
 class Point:
     x: int
     y: int
+
 
 class Floor:
     def __init__(self, red_tiles: List[Point]):
@@ -15,8 +15,9 @@ class Floor:
     def find_largest_area(self) -> int:
         # Cross product of all tiles to find the largest area?
         largest_area = 0
-        for tile in self.red_tiles:
-            for other in self.red_tiles:
+        for i, tile in enumerate(self.red_tiles):
+            for j in range(i, len(self.red_tiles)):
+                other = self.red_tiles[j]
                 area = abs(tile.x - other.x + 1) * abs(tile.y - other.y + 1)
                 if area > largest_area:
                     largest_area = area
@@ -34,9 +35,7 @@ class RedTileParser:
                 continue
 
             coordinates = line.split(",")
-            tile = Point(
-                int(coordinates[0]), int(coordinates[1])
-            )
+            tile = Point(int(coordinates[0]), int(coordinates[1]))
             red_tiles.append(tile)
 
         return Floor(red_tiles)
