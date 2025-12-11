70f7f9ce37db568a155db02c4ac4afb0b207ac44
Robert Lathrop <rldifts@gmail.com>
2025-12-11 13:01:16 -0500

    Setup day 10 part 2

--- aoc25/ten/solution.py
+++ aoc25/ten/solution.py
@@ -30,11 +30,27 @@ class IndicatorLights:
         on_lights = [i for i, l in enumerate(self.lights) if l]
         return self.goal == on_lights
 
+class JoltagePanel:
+    joltages: List[int]
+    goal: List[int]
+
+    def __init__(self, goal: List[int]):
+        self.goal = goal
+        # initialize joltages to 0
+        self.joltages = [0] * len(goal)
+
+    def increment(self, index: int):
+        self.joltages[index] = not self.joltages[index]
+
+    def test(self) -> bool:
+        return self.goal == self.joltages
+
 
 @dataclasses.dataclass
 class Machine:
     lights: IndicatorLights
     buttons: List[Button]
+    joltage: JoltagePanel
 
     def find_min_button_presses(self):
         valid_heights = []
@@ -77,6 +93,48 @@ class Machine:
         for m in button.values:
             self.lights.flip(m)
 
+    def find_min_button_presses_joltage(self):
+        # this is more complicated as we can press each button multiple times
+        valid_heights = []
+        height = 0
+
+        def press_button(index: int):
+            # try pressing, then not pressing, then go to the next button in each case
+            if index >= len(self.buttons):
+                return
+
+            nonlocal height
+            # Press sequence
+            height += 1
+            self.press_button(self.buttons[index])
+            matches = self.lights.test()
+            if matches:
+                valid_heights.append(height)
+            else:
+                # move onto the next button if we don't match
+                press_button(index + 1)
+
+            # Reset from press sequence
+            height -= 1
+            self.press_button(self.buttons[index])
+            # no need to check further with off, the best we could do is tie this result
+            if matches:
+                return
+
+            # Try next buttons again, with us off this time
+            press_button(index + 1)
+
+        press_button(0)
+
+        if valid_heights:
+            return min(valid_heights)
+
+        raise ValueError
+
+    def press_button_joltage(self, button: Button):
+        for m in button.values:
+            self.joltage.increment(m)
+
 
 class DiagramParser:
 
@@ -88,24 +146,24 @@ class DiagramParser:
                 continue
 
             # find indicators, which are surrounded by brackets
-            close_bracket_index = line.index("]")
-            light_goals = line[1:close_bracket_index]
+            light, *rest = line.split("]")
             lights = IndicatorLights(
-                [i for i, c in enumerate(light_goals) if c == "#"], len(light_goals)
+                [i for i, c in enumerate(light[1:]) if c == "#"], len(light) - 1
             )
 
-            # find buttons - can by many
+            button_split, *rest = "".join(rest).split("{")
+            # find buttons - can be many
             buttons = []
-            index = close_bracket_index + 1
-            buttons_groups = line[index:].split(" ")
+            buttons_groups = button_split.split(" ")
             for button_str in buttons_groups:
                 if not button_str.strip() or ")" not in button_str:
                     continue
                 end_index = button_str.index(")")
                 button = Button([int(b) for b in button_str[1:end_index].split(",")])
                 buttons.append(button)
-            # ignore joltage for part 1
-            machines.append(Machine(lights, buttons))
+
+            joltage = JoltagePanel([int(joltage) for joltage in "".join(rest)[:-1].split(",")])
+            machines.append(Machine(lights, buttons, joltage))
         return machines
 
 
@@ -114,7 +172,7 @@ def run() -> int:
     with open("input.txt", "r") as f:
         machines = DiagramParser().parse(f.read())
 
-    return sum(machine.find_min_button_presses() for machine in machines)
+    return sum(machine.find_min_button_presses_joltage() for machine in machines)
 
 
 if __name__ == "__main__":
--- aoc25/ten/test_solution.py
+++ aoc25/ten/test_solution.py
@@ -41,11 +41,40 @@ def test_part_one_third():
     assert machines[2].find_min_button_presses() == 2
 
 
-# def test_part_two_example():
-#     parser = DiagramParser()
-#     example = """[.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}
-# [...#.] (0,2,3,4) (2,3) (0,4) (0,1,2) (1,2,3,4) {7,5,12,7,2}
-# [.###.#] (0,1,2,3,4) (0,3,4) (0,1,2,4,5) (1,2) {10,11,11,5,10,5}"""
-#     diagram = parser.parse(example)
-#
-#     assert diagram.find_minimum_button_presses() == 50
+def test_part_two_example():
+    parser = DiagramParser()
+    example = """[.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}
+[...#.] (0,2,3,4) (2,3) (0,4) (0,1,2) (1,2,3,4) {7,5,12,7,2}
+[.###.#] (0,1,2,3,4) (0,3,4) (0,1,2,4,5) (1,2) {10,11,11,5,10,5}"""
+    machines = parser.parse(example)
+
+    assert [machine.find_min_button_presses_joltage() for machine in machines] == [10, 12, 11]
+
+
+def test_part_two_first():
+    parser = DiagramParser()
+    example = """[.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}
+[...#.] (0,2,3,4) (2,3) (0,4) (0,1,2) (1,2,3,4) {7,5,12,7,2}
+[.###.#] (0,1,2,3,4) (0,3,4) (0,1,2,4,5) (1,2) {10,11,11,5,10,5}"""
+    machines = parser.parse(example)
+
+    assert machines[0].find_min_button_presses_joltage() == 10
+
+
+def test_part_two_second():
+    parser = DiagramParser()
+    example = """[.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}
+[...#.] (0,2,3,4) (2,3) (0,4) (0,1,2) (1,2,3,4) {7,5,12,7,2}
+[.###.#] (0,1,2,3,4) (0,3,4) (0,1,2,4,5) (1,2) {10,11,11,5,10,5}"""
+    machines = parser.parse(example)
+
+    assert machines[1].find_min_button_presses_joltage() == 12
+
+def test_part_two_third():
+    parser = DiagramParser()
+    example = """[.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}
+[...#.] (0,2,3,4) (2,3) (0,4) (0,1,2) (1,2,3,4) {7,5,12,7,2}
+[.###.#] (0,1,2,3,4) (0,3,4) (0,1,2,4,5) (1,2) {10,11,11,5,10,5}"""
+    machines = parser.parse(example)
+
+    assert machines[2].find_min_button_presses_joltage() == 11
